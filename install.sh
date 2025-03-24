ad2c4884f84869769b3d7ce3e16e475f20302e8b
Christopher Tilley <ctilley83@me.com>
2025-03-24 08:23:12 -0400

    har

--- install.sh
+++ install.sh
@@ -633,6 +633,7 @@ hyprsunset() {
     fi
 
     # Configure and build the project using CMake
+    git clean -fdX ./protocols
     cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
     cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
 
